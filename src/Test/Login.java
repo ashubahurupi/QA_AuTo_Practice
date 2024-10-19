a2311d4b17e024babe2f8cc0eaad6858f434c81b
Ashu <Ashish@gmail.com>
2024-10-19 10:41:18 +0530

    Ashish is committing new file;

---
+++ src/Test/Login.java
@@ -0,0 +1,14 @@
+package Test;
+
+import Methods.MainMethodDemo;
+
+public class Login {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("New Class created");
+	}
+
+}
